15f1bc652891c772b6f18c49232ddae8b1a23882
raylandaalman <raylan.websites+git@proton.me>
2024-10-13 19:44:00 +0000

    first chunk of Lua

---
+++ chapter-1/exercise-01.lua
@@ -0,0 +1,36 @@
+-- Printing Hello Wolrd is pretty simple, similar to Python.
+
+print("\nHello World!\n")
+
+
+
+-- different kinds of tables and data types:
+
+my_array = {} -- empty table
+
+my_array['first'] = 1
+
+my_array['second'] = 2
+
+my_array['second'] = false
+
+type(44.3) -- number
+
+type(false) --boolean
+
+type("Lua") -- string
+
+type(my_array) -- table
+
+wait_time = 15
+patience = 20
+
+print("Patience: ", patience)
+
+if wait_time > 10 then patience = 0 end
+
+print("Patience: ", patience)
+
+wait_time = 10
+
+
