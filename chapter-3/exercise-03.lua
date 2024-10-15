dc12de969eda1cb836382a18b07d3d28a7eaf5fc
raylandaalman <raylan.websites+git@proton.me>
2024-10-15 01:04:33 +0000

    finished variables

---
+++ chapter-3/exercise-03.lua
@@ -0,0 +1,31 @@
+
+-- variable initialization tests
+
+local d, f = 5, 10
+print("d: ", d, "\nf: ", f)
+
+d, f = 5, 10;
+print("\nd: ", d, "\nf: ", f)
+
+d, f = 10
+print("\nd: ", d, "\nf: ", f, "\n")
+
+
+
+-- Variable definition
+local a, b
+
+-- Variable Initialization
+a = 10
+b = 30
+print("value of a:", a)
+print("value of b:", b)
+
+-- Swapping of variables
+
+b, a = a, b
+print("value of a:", a)
+print("value of b:", b)
+
+f = 70.0/3.0
+print("value of f:", f)
