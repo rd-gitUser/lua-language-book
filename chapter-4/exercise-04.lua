ec8aab93080a3b18b1208cabe8a76a2ffd84a691
raylandaalman <raylan.websites+git@proton.me>
2024-10-16 01:19:49 +0000

    adding chapter 4 and tables

---
+++ chapter-4/exercise-04.lua
@@ -0,0 +1,42 @@
+print(type("What is my type")) --> string
+
+t = 10
+
+print(type(5.8*t))
+
+print(type(true))
+
+print(type(print))
+
+print(type(nil))
+
+print(type(type(ABC)))
+
+print("\n")
+
+
+-- try the example ot understand operations
+
+a = 21
+b = 10
+
+c = a + b
+print("Line 1 - Value of c is", c) -- 31
+
+c = a - b
+print("Line 2 - Value of c is", c) -- 11
+
+c = a * b
+print("Line 3 - Value of c is", c) -- 210
+
+c = a / b
+print("Line 4 - Value of c is", c) -- 2.1
+
+c = a % b
+print("Line 5 - Value of c is", c) -- 1
+
+c = a^2
+print("Line 6 - Value of c is", c) -- 441
+
+c = -a
+print("Line 4 - Value of c is", c) -- -21
