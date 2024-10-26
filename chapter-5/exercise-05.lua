84e277f478b4499595dd1b9df22b3949c0d24b27
raylandaalman <git@rd-domain.io>
2024-10-26 18:17:42 +0000

    some loops updated

---
+++ chapter-5/exercise-05.lua
@@ -0,0 +1,21 @@
+a = 10
+while (a < 20)
+do
+  print("The value of a: ", a)
+  a = a + 1
+end
+
+print("")
+
+for i = 10, 1, -1
+do
+  print("The value of i:", i)
+end
+
+print("")
+
+b = 10
+repeat
+  print("The value of b: ", b)
+  b = b - 1
+until(b == 0)
