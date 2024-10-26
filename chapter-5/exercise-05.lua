2db6ba86b1deed06db188b223e6b580e7302f174
raylandaalman <git@rd-domain.io>
2024-10-26 19:46:53 +0000

    finished loops

--- chapter-5/exercise-05.lua
+++ chapter-5/exercise-05.lua
@@ -19,3 +19,29 @@ repeat
   print("The value of b: ", b)
   b = b - 1
 until(b == 0)
+
+print ("")
+
+for i = 0, 3, 1
+  do
+    for b = 0, 10, 1
+      do
+        print("Value of b: ", b)
+    end
+    print("value of i: ", i, "\n")
+  end
+
+print ("")
+
+a = 10
+
+while(a < 20)
+  do
+    print("Value of a: ", a)
+    a = a + 1
+    if (a > 15)
+      then
+        break
+    end
+  end
+
