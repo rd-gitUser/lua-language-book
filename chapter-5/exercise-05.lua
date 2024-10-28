65f37890944b4571ccb9cc98f4b5236affb000f2
raylandaalman <git@rd-domain.io>
2024-10-28 20:32:42 +0000

    refactor

--- chapter-5/exercise-05.lua
+++ chapter-5/exercise-05.lua
@@ -1,14 +1,12 @@
 a = 10
-while (a < 20)
-do
+while (a < 20) do
   print("The value of a: ", a)
   a = a + 1
 end
 
 print("")
 
-for i = 10, 1, -1
-do
+for i = 10, 1, -1 do
   print("The value of i:", i)
 end
 
@@ -22,42 +20,35 @@ until(b == 0)
 
 print ("")
 
-for i = 0, 3, 1
-  do
-    for b = 0, 10, 1
-      do
-        print("Value of b: ", b)
-    end
-    print("value of i: ", i, "\n")
+for i = 0, 3, 1 do
+  for b = 0, 10, 1 do
+    print("Value of b: ", b)
   end
+print("value of i: ", i, "\n")
+end
 
 print ("")
 
 a = 10
 
-while(a < 20)
-  do
-    print("Value of a: ", a)
-    a = a + 1
-    if (a > 15)
-      then
-        break
+while(a < 20) do
+  print("Value of a: ", a)
+  a = a + 1
+  if (a > 15) then
+    break
+  end
+end
+
+
+for i = 0, 3, 1 do
+  for b = 0, 10, 1 do
+    if (b < 5) then
+      print("Value of b: ", b)
+    else
+      break
     end
   end
-
-
-for i = 0, 3, 1
-  do
-    for b = 0, 10, 1
-      do
-        if (b < 5)
-          then
-            print("Value of b: ", b)
-          else
-            break
-        end
-      end
-    print("Value of i: ", i, "\n")
-  end
+  print("Value of i: ", i, "\n")
+end
 
 
