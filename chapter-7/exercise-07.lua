bd7b3d2fea205de58d1ed739d054eda90449084a
raylandaalman <git@rd-domain.io>
2024-10-29 01:20:28 +0000

    Functions done, some Strings

--- chapter-7/exercise-07.lua
+++ chapter-7/exercise-07.lua
@@ -18,4 +18,47 @@ end
 a = 5
 b = 10
 
-print("The max number between ", a , "and", b, "is: ", max(a,b))
+print("The max number between ", a , "and", b, "is: ", max(a,b));
+
+print("\n");
+
+myprint = function(param)
+  print("This is my print function - ##",param,"##")
+end
+
+function add(num1, num2, functionPrint)
+  result = num1 + num2;
+  functionPrint(result)
+end
+
+myprint(10);
+add(2,5,myprint);
+
+print("\n");
+
+function average(...)
+  result = 0;
+  local arg = {...}
+  print(arg);
+  for i, v in ipairs(arg) do
+    print("value of i and v: ",i,v);
+    result = result + v;
+  end
+  return result/#arg
+end
+print("The average is: ",average(10,5,3,4,5,6));
+
+print("\n");
+
+string1 = "Lua";
+print("\"String1 is: \"",string1);
+
+string2 = 'Tutorial';
+print("String2 is: ",string2);
+
+string3 = [["Lua Tutorial"]];
+print("String 3 is",string3);
+
+print("\n");
+
+
