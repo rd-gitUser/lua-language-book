ecf4ce4a78a30788f8028d8578d2a901b00daa87
raylandaalman <git@rd-domain.io>
2024-10-31 02:41:34 +0000

    stateless iteration

---
+++ chapter-8/exercise-08.lua
@@ -0,0 +1,87 @@
+-- arrays
+
+array = {"Lua","Language"};
+
+for i = 0, 2 do
+  print(array[i]);
+end
+
+print("\n");
+
+array = {};
+for i = -2, 2 do
+  array[i] = i * 2;
+end
+
+for i = -2, 2 do
+  print(array[i]);
+end
+
+print("\n");
+
+-- Multidimensional arrays
+
+array = {};
+maxRows = 3;
+maxColumns = 3;
+
+for row=1,maxRows do
+  for col=1,maxColumns do
+    array[row * maxColumns + col] = row * col
+  end
+end
+
+for row = 1,maxRows do
+  for col = 1,maxColumns do
+    print(array[row*maxColumns+col]);
+  end
+end
+
+-- finding the content of the array
+
+for i = 1,12 do
+  print("Index "..i..": ",array[i]);
+end
+
+
+print("\n");
+
+
+array = {"Lua", "Tutorial"}
+
+for key,value in ipairs(array) do
+  print(key,value);
+end
+
+
+print("\n");
+
+
+function square(iteratorMaxCount,currentNumber)
+  if currentNumber < iteratorMaxCount then
+    currentNumber = currentNumber + 1;
+    return currentNumber, currentNumber*currentNumber;
+  end
+end
+
+for i, n in square,3,0 do
+  print(i,n)
+end
+
+
+print("\n");
+
+-- rewrite as a function that returns a function
+
+function squares(iteratorMaxCount)
+  return square,iteratorMaxCount,0;
+end
+
+for i, n in squares(3) do
+  print(i,n);
+end
+
+
+
+
+
